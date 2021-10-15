44078db5f6e41e1623449b57d149fc284565e9df
Dickson <pythonscript254@gmail.com>
2021-10-15 11:29:11 +0300

    add array of quote section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,6 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles: Article[]
+  constructor() {
+    this.articles =[
+      new Article('Angular', 'http://angular.io', 3),
+      new Article('fullstack', 'http://fullstack.io', 2),
+      new Article('Angular homepage', 'http://angular.io', 1),
+    ]
+   }
+
+
+
+
+  
  add_article(title: HTMLInputElement, link: HTMLInputElement){
    console.log(title.value)
 
